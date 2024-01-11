69f1cbfbae44420b2c693485492e24c564b7a21c
LuanaMonteros <luamonteros08@gmail.com>
2024-01-11 19:29:02 -0300

    Co-authored-by: matiases1995@outlook.es <matiases1995@outlook.es> Co-authored-by: Lucas Leguizamón <Kalusa93@users.noreply.github.com>

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+const bcrypt = require('bcrypt');
+
+const userFilePath = path.join(__dirname, "../data/usersDateBase.json");
+const users = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
+
+
 const usersController = {
     registerController: (req, res) => {
         res.render('users/register', {title: 'Registrate', css: 'register.css'});
     },
     loginController: (req, res) => {
         res.render('users/login', {title: 'Ingresá', css: 'login.css'});
+    },
+    addUserController: (req, res) => {
+
+        const image = req.file ? req.file.filename : "default-image.png";
+
+
+        delete req.body.confirmpassword
+
+        const newUser = {
+            id: users.length + 1,
+            ...req.body,
+            password: bcrypt.hashSync(req.body.password, 10),
+            image,
+        }
+
+        users.push(newUser);
+
+        fs.writeFileSync(userFilePath, JSON.stringify(users, null, ' '));
+
+        res.redirect("/");
+        
     }
 };
+
 
 module.exports = usersController;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,35 @@
 const controller = require('../controllers/usersController');
 const { Router } = require('express');
 const router = Router();
+const multer = require('multer');
+const path = require('path');
+
+// MULTER - Manejo del almacenamiento
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, "../../public/images/users"));
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + "-" + file.originalname);
+    },
+});
 
 const routes = {
     registerRoute: '/register',
     loginRoute: '/login',
 };
 
+
+
+
+const upload = multer({ storage: storage });
+
 router.get(routes.registerRoute, controller.registerController);
+router.post(routes.registerRoute, upload.single('image'), controller.addUserController);
 
 router.get(routes.loginRoute, controller.loginController);
+
+
+
 
 module.exports = router;
