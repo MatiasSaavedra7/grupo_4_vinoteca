4c9c3c30035162eb1de81b2527bb21c25dbfe783
Santiago Almaraz <santyagoalmaraz@gmail.com>
2024-01-23 00:55:12 -0300

    cambios parciales al header

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -18,7 +18,7 @@ function userLoggedMiddleware(req, res, next) {
 	if (req.session.userLogged) {
 		res.locals.isLogged = true;
 		res.locals.userLogged = req.session.userLogged;
-		console.log(req.session);
+		// console.log(req.session);
 	}
 
 	next();
