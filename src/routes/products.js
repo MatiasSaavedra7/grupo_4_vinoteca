e77bb6a2e8945dceb4f2c0cb54074e3ebf7193a4
matiases1995 <matiassaavedra1995@gmail.com>
2024-02-16 23:52:53 -0300

    add adminMiddleware

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,8 @@ const controller = require('../controllers/productsController');
 const { Router } = require('express');
 const router = Router();
 const {upload} = require("../middlewares/multer");
+const adminMiddleware = require('../middlewares/adminMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // GET PRODUCTS
 router.get('/', controller.products);
@@ -14,18 +16,18 @@ router.get('/imported', controller.imported);
 router.get('/detail/:id', controller.detail);
 
 // ADD PRODUCTS
-router.get('/add', controller.add);
+router.get('/add', authMiddleware, adminMiddleware, controller.add);
 //! AGREGAR MIDDLEWARE PARA PERMISOS DE ADMIN!!!!!!!!!
-router.post('/add', upload.single('image'), controller.create);
+router.post('/add', adminMiddleware, upload.single('image'), controller.create);
 
 //EDIT PRODUCTS
-router.get('/edit/:id', controller.edit);
+router.get('/edit/:id', authMiddleware, adminMiddleware, controller.edit);
 
-router.put('/edit/:id', upload.single('image'), controller.update);
+router.put('/edit/:id', authMiddleware, adminMiddleware, upload.single('image'), controller.update);
 
 // DELETE PRODUCT
-router.get('/delete/:id', controller.delete)
+router.get('/delete/:id', authMiddleware, adminMiddleware, controller.delete)
 
-router.delete('/delete/:id', controller.destroy);
+router.delete('/delete/:id', authMiddleware, adminMiddleware, controller.destroy);
 
 module.exports = router;
