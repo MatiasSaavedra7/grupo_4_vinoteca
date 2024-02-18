75c8bb69c780460bda12d229f2cc4aa4c2a310fc
ivana <lunaivana01@gmail.com>
2024-02-17 22:52:55 -0300

    creo login service y login controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,128 +2,91 @@ const fs = require("fs");
 const path = require("path");
 const bcryptjs = require("bcryptjs");
 const { validationResult } = require("express-validator");
-const usersService = require('../model/services/usersService');
+const usersService = require("../model/services/usersService");
 const { log } = require("console");
 
-// const userFilePath = path.join(__dirname, "../data/usersDataBase.json");
-// const users = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
 
 const usersController = {
-	register: (req, res) => {
-		res.render("users/register");
-	},
+  register: (req, res) => {
+    res.render("users/register");
+  },
 
-	login: (req, res) => {
-		res.render("users/login");
-	},
+  login: (req, res) => {
+    res.render("users/login");
+  },
 
-	addUser: async (req, res) => {
-		//Validamos los datos
-		const errors = validationResult(req);
+  addUser: async (req, res) => {
+    //Validamos los datos
+    const errors = validationResult(req);
 
-		if (!errors.isEmpty()) {
-			return res.render("users/register", {
-				errors: errors.mapped(),
-				old: req.body,
-			});
-		}
+    if (!errors.isEmpty()) {
+      return res.render("users/register", {
+        errors: errors.mapped(),
+        old: req.body,
+      });
+    }
 
-		delete req.body.confirmpassword;
+    delete req.body.confirmpassword;
 
-		//Verificamos si el usuario subio una imagen
-		const image = req.file ? req.file.filename : "default-image.png";
+    //Verificamos si el usuario subio una imagen
+    const image = req.file ? req.file.filename : "default-image.png";
 
-		//Creamos al nuevo usuario
-		/* const newUser = {
-			id: users.length + 1,
-			...req.body,
-			password: bcryptjs.hashSync(req.body.password, 10),
-			image,
-		}; */
-		try {
-			let newUser = await usersService.add(req.body, req.email, image)
-			console.log(newUser);
-		} catch (error) {
-			console.log(error.message);
-		}
+    //Creamos al nuevo usuario
+    try {
+      let newUser = await usersService.add(req.body, image);
+      console.log(newUser);
+    } catch (error) {
+      console.log(error.message);
+    }
 
-		//Lo agregamos al array de objetos
-		// users.push(newUser);
+    //Redireccionamos al home
+    res.redirect("/users/login");
+  },
 
-		//Pasamos el array a json y guardamos
-		// fs.writeFileSync(userFilePath, JSON.stringify(users, null, " "));
+  loginProcess: async (req, res) => {
+    try {
+      //Asignamos a errors los resultados de las validaciones.
+      const errors = validationResult(req);
 
-		//Redireccionamos al home
-		res.redirect("/users/login");
+      //Verificamos si errors no esta vacia
+      if (!errors.isEmpty()) {
+        //Retornamos a la vista login los errores
+        return res.render("users/login", { errors: errors.mapped() });
+      }
 
+      let userFind = await usersService.log(req.body);
+      //Guardamos al usuario en session
+      req.session.userLogged = userFind;
 
-	},
+      if (req.body.remember) {
+        res.cookie("userEmail", req.body.email, {
+          maxAge: 1000 * 30,
+        });
+      }
+	  //Retornamos al perfil una vez validados el email y password
+      return res.redirect("/users/profile");
+    } catch (error) {
+		console.error(error)
+      return res.render("users/login", {
+        errors: {
+          email: {
+            msg: error,
+          },
+        },
+      });
+    }
+  },
 
-	loginProcess: (req, res) => {
-		//Asignamos a errors los resultados de las validaciones.
-		const errors = validationResult(req);
+  profile: (req, res) => {
+    res.render("users/profile", { user: req.session.userLogged });
+  },
 
-		//Verificamos si errors no esta vacia
-		if (!errors.isEmpty()) {
-			//Retornamos a la vista login los errores
-			return res.render("users/login", { errors: errors.mapped() });
-		}
+  logout: (req, res) => {
+    res.clearCookie("userEmail");
+    req.session.destroy();
 
-		//Buscamos al usuario a traves de su email
-		const userFilePath = path.join(__dirname, "../data/usersDataBase.json");
-		const users2 = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
-
-		const userFind = users2.find((u) => u.email == req.body.email);
-
-		if (userFind) {
-			//Si existe el usuario verificamos la contraseña
-			if (bcryptjs.compareSync(req.body.password, userFind.password)) {
-				//En caso afirmativo borramos la contraseña del usuario
-				delete userFind.password;
-
-				//Guardamos al usuario en session
-				req.session.userLogged = userFind;
-
-				if (req.body.remember) {
-					res.cookie("userEmail", req.body.email, {
-						maxAge: 1000 * 30,
-					});
-				}
-
-				//Retornamos a la home una vez validados el email y password
-				return res.redirect("/users/profile");
-			} else {
-				//En caso de que la contraseña sea incorrecta mostramos un mensaje
-				return res.render("users/login", {
-					errors: {
-						email: {
-							msg: "Las credenciales no coinciden",
-						},
-					},
-				});
-			}
-		}
-
-		//En caso de que el correo se no encuentre mostramos este mensaje
-		return res.render("users/login", {
-			errors: {
-				email: {
-					msg: "Correo no encontrado :(",
-				},
-			},
-		});
-	},
-
-	profile: (req, res) => {
-		res.render("users/profile", { user: req.session.userLogged });
-	},
-
-	logout: (req, res) => {
-		res.clearCookie("userEmail");
-		req.session.destroy();
-
-		res.redirect("/");
-	},
+    res.redirect("/");
+  },
 };
 
 module.exports = usersController;
