728f7475a6c2030ae80942823f94f7fbb8e2ddb4
Santiago Almaraz <santyagoalmaraz@gmail.com>
2024-04-06 16:08:43 -0300

    agrego campo rol_id a la api de usuarios

--- src/model/services/apiServices/apiUserService.js
+++ src/model/services/apiServices/apiUserService.js
@@ -28,7 +28,7 @@ module.exports = {
     getById: async function (id) {
         try {
             return await db.User.findByPk(id, {
-                attributes: ['id', 'firstName', 'lastName', 'email', 'image'], // Especificamos las columnas requeridas
+                attributes: ['id', 'firstName', 'lastName', 'email', 'image', 'rol_id'], // Especificamos las columnas requeridas
             });
         } catch (error) {
             return [];
